9160ab7cf2346f88c2b4023e0147e661d823c3cd
Brian Karcher <briankarcher90630@gmail.com>
2026-05-15 19:12:20 -0700

    Imgui added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
-//#include <imgui.h>
-//#include <backends/imgui_impl_sdl2.h>
-//#include <backends/imgui_impl_opengl3.h>
+#include <imgui.h>
+#include <backends/imgui_impl_sdl2.h>
+#include <backends/imgui_impl_opengl3.h>
 #include <SDL2/SDL_opengl.h>
 #include <zip.h>
 #include <stdio.h>
